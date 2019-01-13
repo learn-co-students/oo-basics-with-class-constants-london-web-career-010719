96c46b2de0bed8d88fdc545583a735d466a428b0
Flatiron <flatiron@flatirons-air-2.home>
2019-01-13 17:33:45 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,13 +2,19 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    # if the array does not already include the brand --> push it into the array.
+    if BRANDS.include?(brand)
+    else
+      BRANDS.push(brand)
+    end
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
 end
